2bbef2d35bc94e99a9d5a53bd6709ec4f485f7b2
Manish <90108550+manish-project@users.noreply.github.com>
2023-07-02 00:33:44 +0530

    Added Javascript file.

---
+++ index.js
@@ -0,0 +1,24 @@
+let string= "";
+let buttons = document.querySelectorAll("button");
+let input = document.getElementById("inputBox");
+let arr = Array.from(buttons);
+
+arr.forEach(button =>{
+    button.addEventListener('click', (e)=>{
+       if( e.target.innerHTML == '='){
+        string = eval(string);
+        input.value= string;
+       } else if(e.target.innerHTML == 'AC'){
+string= ''
+input.value = string;
+       }
+       else if(e.target.innerHTML == 'DL'){
+        string = string.substr(0, string.length-1);
+        input.value = string;
+       }
+       
+       else{
+       string += e.target.innerHTML;
+       input.value = string;}
+    })
+})
